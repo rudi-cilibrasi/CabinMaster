f3efc17a1626f5e3ed511c4486775ab9cc8b287b
Rudi Cilibrasi <cilibrar@gmail.com>
2022-12-17 15:20:20 -0800

    more tutorial

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CabinMaster
-Simple game to learn HTML / JS with daughter
+## Simple game to learn HTML / JS with daughter.
+## By Vanessa Cilibrasi and Rudi Cilibrasi
+
+## Try it at
+
+# https://rudi-cilibrasi.github.io/CabinMaster/
--- script.js
+++ script.js
@@ -3,20 +3,33 @@ var xCo, yCo;
 var mouseX, mouseY;
 
 var hitBoxMainMenuNew = [6, 130, 211, 192];
+var hitBoxMainMenuQuit = [4, 453, 214, 539];
 var hitBoxBackArrowWelcome = [41, 31, 123, 87];
 var hitBoxStartTutorial = [278, 431, 518, 521];
+var hitBoxTutorialFullScreen = [1, 1, 693, 693];
 var currentPlace = "main_menu";
 
 var stateTree = {
     "main_menu": {
         "hitboxes": [
-            [hitBoxMainMenuNew, doHitBoxMainMenuNew]
+            [hitBoxMainMenuNew, doHitBoxMainMenuNew],
+            [hitBoxMainMenuQuit, doHitBoxMainMenuQuit]
         ],
     },
     "new_click_welcome": {
         "hitboxes": [
             [hitBoxBackArrowWelcome, doHitBoxBackArrowWelcome],
             [hitBoxStartTutorial, doHitBoxStartTutorial]
+        ],
+    },
+    "tutorial_01": {
+        "hitboxes": [
+            [hitBoxTutorialFullScreen, doHitBoxTutorial_01]
+        ],
+    },
+    "tutorial_02": {
+        "hitboxes": [
+            [hitBoxTutorialFullScreen, doHitBoxTutorial_02]
         ],
     },
 }
@@ -49,6 +62,11 @@ function doHitBoxMainMenuNew() {
     moveToState("new_click_welcome");
 }
 
+function doHitBoxMainMenuQuit() {
+    mainView.src = null;
+    currentPlace = "quit";
+}
+
 function doHitBoxBackArrowWelcome() {
     moveToState("main_menu");
 }
@@ -57,8 +75,17 @@ function doHitBoxStartTutorial() {
     moveToState("tutorial_01");
 }
 
+function doHitBoxTutorial_01() {
+    moveToState("tutorial_02");
+}
+
+function doHitBoxTutorial_02() {
+    moveToState("tutorial_03");
+}
+
 function processMouseDown() {
-    var currentHitBoxes = stateTree[currentPlace]["hitboxes"];
+    var currentStateValue = stateTree[currentPlace] ?? {};
+    var currentHitBoxes = currentStateValue["hitboxes"] ?? [];
     for (var i = 0; i < currentHitBoxes.length; i++) {
         if (checkHitBox(currentHitBoxes[i][0])) {
             currentHitBoxes[i][1]();
